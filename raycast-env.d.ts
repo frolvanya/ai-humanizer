601e0204b81eac951394066a570274bfba7bb80f
Ivan Frolov <frolvanya@gmail.com>
2024-11-15 10:41:33 +0100

    chore: added api key

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -7,7 +7,10 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {
+  /** API Key - Enter your Rephrasy.ai API key */
+  "apiKey": string
+}
 
 /** Preferences accessible in all the extension's commands */
 declare type Preferences = ExtensionPreferences
